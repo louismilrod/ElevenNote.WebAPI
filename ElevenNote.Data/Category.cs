e7a3aca28af0a016b6e783eacf97141b10a1598a
Louis Milrod <int4rweb@gmail.com>
2022-01-14 15:38:09 -0500

    created folders for models, created category data type

--- ElevenNote.Data/Category.cs
+++ ElevenNote.Data/Category.cs
@@ -7,21 +7,15 @@ using System.Threading.Tasks;
 
 namespace ElevenNote.Data
 {
-    public class Note
+    public class Category
     {
         [Key]
-        public int NoteId { get; set; }
+        public int CategoryId { get; set; }
         [Required]
-        public Guid OwnerId { get; set; }
-        [Required]
-        public string Title { get; set; }
-        [Required]
-        public string Content { get; set; }
+        [MaxLength(25, ErrorMessage = "You can only have 25 characters per category")]
+        public string Name { get; set; }
         [Required]
         public DateTimeOffset CreatedUtc { get; set; }
         public DateTimeOffset ModifiedUtc { get; set; }
-
-        //[ForeignKey (nameof(???))]
-        //public int CategoryId { get; set; }
     }
 }
